204ccf32b35c2c8eb504de503c5aff76cdfcabc4
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-04-01 12:20:55 -0300

    create a bunch of components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { Header } from './header/header.component';
+import { Recipes } from './recipes/recipes.component';
+import { RecipeDetail } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipesList } from './recipes/recipes-list/recipes-list.component';
+import { ShoppingList } from './shopping-list/shopping-list.component';
+import { ShoppingEdit } from './shopping-list/shopping-edit/shopping-edit.component';
+import { RecipeItem } from './recipes/recipes-list/recipe-item/recipe-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Header
+    Header,
+    Recipes,
+    RecipeDetail,
+    RecipesList,
+    RecipeItem,
+    ShoppingList,
+    ShoppingEdit
   ],
   imports: [
     BrowserModule
