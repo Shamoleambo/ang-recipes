97a82739e00c6d929a7139227b952ba4bcd23d12
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-04-02 10:49:56 -0300

    replicate list items using ngFor

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,9 +1,16 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
-    selector: 'app-recipes-list',
-    templateUrl: './recipes-list.component.html'
+  selector: 'app-recipes-list',
+  templateUrl: './recipes-list.component.html',
 })
 export class RecipesList {
-
+  recipes: Recipe[] = [
+    new Recipe(
+      'Plastic Food',
+      'Stupid plastic food',
+      'https://m.media-amazon.com/images/I/81tGII2MOmL.jpg'
+    ),
+  ];
 }
