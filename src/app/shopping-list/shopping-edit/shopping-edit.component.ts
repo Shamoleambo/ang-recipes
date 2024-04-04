2ee8eecd49341d5bec2cecc136ec33630942baaf
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-04-04 08:56:57 -0300

    finish shopping-cart list component

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -2,7 +2,12 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-shopping-edit',
-    templateUrl: './shopping-edit.component.html'
+    templateUrl: './shopping-edit.component.html',
+    styles: [`
+        .btn {
+            margin: 0 1em;
+        }
+    `]
 })
 export class ShoppingEdit {
 
