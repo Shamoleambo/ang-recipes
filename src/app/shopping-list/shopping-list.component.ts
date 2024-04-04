788331b4751588663fddcd8eab2958721177b147
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-04-04 08:45:41 -0300

    create a sequence of ingredients links using ngFor

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,9 +1,13 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
-    selector: 'app-shopping-list',
-    templateUrl: './shopping-list.component.html'
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
 })
 export class ShoppingList {
-
+  ingredients: Ingredient[] = [
+    new Ingredient('Apple', 5),
+    new Ingredient('Tomatoes', 6),
+  ];
 }
